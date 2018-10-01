f5e6ff021b126bf264625ef11dfd611e82fe6287
Luke Nathan <luke.nathan@LukeLuke.local>
2018-10-01 09:25:59 -0400

    strings and numbers

---
+++ LoopDemo.py
@@ -0,0 +1,13 @@
+str = "Monkey"
+
+for i in range(0, len(str)):
+    print(str[i])
+
+print("***********")
+
+str2 = ("Monkey")[::-1]
+print(str2)
+
+print("************")
+for i in range(0, 3):
+    print(str[i * 2])
