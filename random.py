1dd09121f4046f7bae0a22fc03de6fc60702c439
Luke Nathan <luke.nathan@LukeLuke.local>
2018-11-15 13:36:08 -0500

    update misp

---
+++ random.py
@@ -0,0 +1,8 @@
+import random
+# imports widget "random"
+
+random.randint(1, 5)
+# generates a random number between 1 and 5
+if random.randint(1, 5) == 1:
+    # checks if the number generated is 1
+    print("1")
