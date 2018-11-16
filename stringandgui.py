d4fdda8964a999dbcf33826f97302b278b0f0b05
Luke Nathan <luke.nathan@LukeLuke.local>
2018-11-15 22:51:35 -0500

    gui example

---
+++ stringandgui.py
@@ -0,0 +1,9 @@
+
+import tkinter as tk
+
+root = tk.Tk()
+
+water = tk.Label(root, text="Water is made up of 2 hydrogens and one oxygens. \n Vital for all known life forms, water is a transparent, tasteless, \n and odorless chemical substance with a boiling point of 100°C")
+water.pack()
+
+root.mainloop()
